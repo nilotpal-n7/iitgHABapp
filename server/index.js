29e1bcd7ef0a7038ba812058bd6fe9d1a06742b6
Md Ayan Hassan <anwar1963alam@gmail.com>
2024-09-02 18:38:17 +0530

    Add different routes in index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const testRoute = require('./modules/test/testRoute');
+const itemRoute = require('./modules/item/itemRoute.js');
+const userRoute = require('./modules/user/userRoute.js');
+const complaintRoute = require('./modules/complaint/complaintRoute.js');
 
 dotenv.config();
 
@@ -28,6 +31,15 @@ app.get('/hello', (req, res) => {
 });
 //test route
 app.use('/api/test', testRoute);
+
+//item route
+app.use('/api/items', itemRoute);
+
+//user route
+app.use('/api/users', userRoute);
+
+//complaint route
+app.use('/api/complaints', complaintRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
