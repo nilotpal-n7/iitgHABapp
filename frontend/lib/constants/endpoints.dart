f4f910e26ecd3fc0b3b862f142b52cc92d53f2c4
yamiamit <way2aamit@gmail.com>
2024-09-09 23:10:48 +0530

    auth flow along with basic ui

---
+++ frontend/lib/constants/endpoints.dart
@@ -0,0 +1,23 @@
+class AuthEndpoints {
+  static const String getAccess = 'https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c/oauth2/v2.0/authorize?client_id=ef3696d9-2ab2-423c-a494-fb0a193e0446&response_type=code&redirect_uri=iitgcomplain://auth&scope=offline_access%20user.read&state=random-state&prompt=consent';
+}
+
+class Userendpoints {
+  static const getdetails = 'https://graph.microsoft.com/v1.0/me';
+}
+
+class clientid {
+  static const Clientid = 'ef3696d9-2ab2-423c-a494-fb0a193e0446';
+}
+
+class tokenlink {
+  static const Tokenlink = 'https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c/oauth2/v2.0/token';
+}
+
+class redirecturi {
+  static const Redirecturi = 'iitgcomplain://auth';
+}
+
+class clientSecretid {
+  static const Clientscret = 'KmD8Q~xCY8cR6RNrCJlUJBCzTbWqx3dKdFVTLajR';
+}
---
+++ frontend/lib/screens/login_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/apis/authentication/login.dart';
+
+import 'dart:io';
+import 'package:frontend/widgets/Login_screen/login_button.dart';
+import 'package:frontend/widgets/common/snack_bar.dart';
+
+class loginScreen extends StatefulWidget {
+  const loginScreen({super.key});
+
+  @override
+  State<loginScreen> createState() => _loginScreenState();
+}
+
+class _loginScreenState extends State<loginScreen> {
+  final Cleanimage = const AssetImage('assets/fonts/clean.png');
+  final logoiitg = const AssetImage('assets/fonts/IITG_logo.png');
+  var _inprogress = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        top: Platform.isAndroid,
+        bottom: Platform.isAndroid,
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: Cleanimage,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center( // Center the content
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/fonts/IITG_logo.png',
+                    height: 100, // Adjust size as needed
+                  ),
+                  const SizedBox(height: 20), // Add some space
+                  const Text(
+                    'IITG MAINTENANCE',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 150), // Add space between text and button
+                  Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Material(
+                      color: Colors.black, // No background color for the container
+                      child: InkWell(
+                        splashColor: Colors.white,
+                        onTap: () async {
+                          try {
+                            setState(() {
+                              _inprogress = true;
+                            });
+                            await authenticate();
+                            setState(() {
+                              _inprogress = false;
+                            });
+                            if (!mounted) return;
+                            showSnackBar('Successfully Logged In', context);
+                          } catch (e) {
+                            setState(() {
+                              _inprogress = false;
+                            });
+                            showSnackBar('Something Went Wrong', context);
+                          }
+                        },
+                        child: Padding(padding:EdgeInsets.all(15),
+                            child:  const LoginButton()),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
