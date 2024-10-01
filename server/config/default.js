3d05f7ed8b67c8d27bbec8d4048cb597f30d221b
yamiamit <way2aamit@gmail.com>
2024-10-01 14:20:56 +0530

    added appConfig

--- server/config/default.js
+++ server/config/default.js
@@ -3,7 +3,8 @@ require("dotenv/config");
 module.exports = {
     port: process.env.PORT || 3000,
     mongoURI: process.env.MONGODB_URI,
-    // don't know about mobileURL and clientURL so omitting this also
+    mobileURL: process.env.MOBILE_URL,
+    // don't know about  clientURL so omitting this also
     jwtSecret: process.env.JWT_SECRET,
     // aeskey unknown here so omitting
     // adminjwtsecret unknown so omitting
