0089fb900c2b14f07ad3d540ed943b82259aa9a4
yamiamit <way2aamit@gmail.com>
2024-10-01 17:28:18 +0530

    added tweeks to authenticateJWT.js

--- server/middleware/authenticateJWT.js
+++ server/middleware/authenticateJWT.js
@@ -1,8 +1,8 @@
 const User = require("../modules/user/userModel.js");
 
 const authenticateJWT = async function (req, res, next) {
-    // const token = req.cookies.token;
-    const token = req.headers?.authorization?.split(" ")[1]
+    const token = req.cookies.token;
+     if(!token) token  = req.headers?.authorization?.split(" ")[1]
     if (!token) return res.status(401).json({message: "Authentication required"});
 
     const user = User.findByJWT(token);
