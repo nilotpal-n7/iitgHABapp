3e3027c943fda1bf7b553ca4f28a91d92a9ec82a
yamiamit <way2aamit@gmail.com>
2024-10-01 19:23:36 +0530

    changed a bug

--- server/middleware/authenticateJWT.js
+++ server/middleware/authenticateJWT.js
@@ -1,10 +1,11 @@
 const User = require("../modules/user/userModel.js");
-const AppError = require('.././utils/appError.js');
+const AppError = require('../utils/appError.js');
 
 const authenticateJWT = async function (req, res, next) {
     let token = req.cookies.token;
     if (!token) token = req.headers?.authorization?.split(" ")[1];
     if (!token) return next(new AppError(403, "Invalid token"));
+    console.log(token);
     const user = await User.findByJWT(token);
     if (!user) return next(new AppError(403, "Not Authenticated"));
     req.user = user;
