15832d7db98d1c77154c623dfa6253d63fc44d2b
yamiamit <way2aamit@gmail.com>
2024-10-03 20:29:28 +0530

    changed client id and secret

--- frontend/lib/constants/endpoints.dart
+++ frontend/lib/constants/endpoints.dart
@@ -6,7 +6,7 @@ class UserEndpoints {
 
 
 class AuthEndpoints {
-  static const String getAccess = 'https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c/oauth2/v2.0/authorize?client_id=ef3696d9-2ab2-423c-a494-fb0a193e0446&response_type=code&redirect_uri=https://iitgcomplaintapp.onrender.com/api/auth/login/redirect/mobile&scope=offline_access%20user.read&state=12345&prompt=consent';
+  static const String getAccess = 'https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c/oauth2/v2.0/authorize?client_id=10097bd9-bd33-4181-aced-b041440acade&response_type=code&redirect_uri=https://iitgcomplaintapp.onrender.com/api/auth/login/redirect/mobile&scope=offline_access%20user.read&state=12345&prompt=consent';
 }
 
 class Userendpoints {
@@ -14,7 +14,7 @@ class Userendpoints {
 }
 
 class clientid {
-  static const Clientid = 'ef3696d9-2ab2-423c-a494-fb0a193e0446';
+  static const Clientid = '10097bd9-bd33-4181-aced-b041440acade';
 }
 
 class tokenlink {
@@ -26,5 +26,5 @@ class redirecturi {
 }
 
 class clientSecretid {
-  static const Clientscret = 'KmD8Q~xCY8cR6RNrCJlUJBCzTbWqx3dKdFVTLajR';
+  static const Clientscret = 'w1h8Q~_.YUQPkHuorB4gaOJJcvkkPYc7qi70nc.B';
 }
