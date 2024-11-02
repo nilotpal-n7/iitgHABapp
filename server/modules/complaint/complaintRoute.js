cdfdcce4efd60335aa4843930c91c5a9749ce197
Md Ayan Hassan <anwar1963alam@gmail.com>
2024-11-02 23:52:31 +0530

    push complaint delete

--- server/modules/complaint/complaintRoute.js
+++ server/modules/complaint/complaintRoute.js
@@ -4,12 +4,14 @@ const complaintRouter = express.Router();
 
 const authenticateJWT = require('../../middleware/authenticateJWT');
 
-const { submitComplaint, updateComplaint, getComplaint } = require('./complaintController');
+const { submitComplaint, updateComplaint, getComplaint, deleteComplaint } = require('./complaintController');
 
-complaintRouter.post('/', authenticateJWT, submitComplaint);
+complaintRouter.post('/', submitComplaint);
 
 complaintRouter.put('/:id', authenticateJWT, updateComplaint);
 
-complaintRouter.get('/:id', getComplaint);
+complaintRouter.get('/:id', authenticateJWT, getComplaint);
+
+complaintRouter.delete('/:id', authenticateJWT, deleteComplaint);   
 
 module.exports = complaintRouter;
