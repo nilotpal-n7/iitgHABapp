8df02778e5d8f56ba3986d119b2f0a90f962e407
Md Ayan Hassan <anwar1963alam@gmail.com>
2024-11-02 12:19:58 +0530

    Change in server code

--- server/modules/item/itemRoute.js
+++ server/modules/item/itemRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const authenticateJWT = require('../../middleware/authenticateJWT.js');
 
-const { getComplaintsOfItemsByHostel, createItem, deleteItem, updateItem, getItems, getItem } = require('./itemController');
+const { getComplaintsOfItemsByHostel, createItem, deleteItem, updateItem, getItems, getItem, getItemsWithComplaints, getItemsForHAB } = require('./itemController');
 
 const itemRouter = express.Router();
 
@@ -15,6 +15,10 @@ itemRouter.put('/:id', authenticateJWT, updateItem);
 
 itemRouter.get('/', authenticateJWT, getItems);
 
-itemRouter.get('/:qr',  getItem);
+itemRouter.get('/:qr', getItem);
+
+itemRouter.get('/:hostel', authenticateJWT, getItemsWithComplaints);
+
+itemRouter.get('/hab/:hostel', authenticateJWT, getItemsForHAB);
 
 module.exports = itemRouter;
