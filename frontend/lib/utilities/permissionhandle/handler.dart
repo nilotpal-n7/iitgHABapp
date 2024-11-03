e6fd8c48263aa0f09d32fd8f3b32b05a7b849ccb
yamiamit <way2aamit@gmail.com>
2024-11-03 17:43:26 +0530

    fixed bug camera not opening after accepting permissions

--- frontend/lib/utilities/permissionhandle/handler.dart
+++ frontend/lib/utilities/permissionhandle/handler.dart
@@ -1,26 +1,22 @@
 import 'package:permission_handler/permission_handler.dart';
-import 'package:flutter/material.dart';
 
-class permission {
-  Future<void> requestCameraPermission() async {
+class PermissionHandler {
+  Future<void> requestCameraPermission(Function onGranted) async {
     var status = await Permission.camera.status;
     if (!status.isGranted) {
-      // If the camera permission is not granted, request it
+      // Request permission if not granted
       status = await Permission.camera.request();
+
       if (status.isGranted) {
-        print("Camera permission granted");
+        onGranted(); // Start the camera if permission is granted
       } else if (status.isDenied) {
-        // Handle permission denial
         print("Camera permission denied");
       } else if (status.isPermanentlyDenied) {
-        // Handle permanent denial (open settings)
         print("Camera permission permanently denied");
         openAppSettings();
       }
+    } else {
+      onGranted(); // Start the camera immediately if permission is already granted
     }
   }
-
 }
-
-
-
