1200b641dab1414e6e48b8baf1845195797c9eff
yamiamit <way2aamit@gmail.com>
2024-11-08 22:54:35 +0530

    added vibration and some ui changes for QRscanner

--- frontend/lib/utilities/permissionhandle/handler.dart
+++ frontend/lib/utilities/permissionhandle/handler.dart
@@ -4,11 +4,11 @@ class PermissionHandler {
   Future<void> requestCameraPermission(Function onGranted) async {
     var status = await Permission.camera.status;
     if (!status.isGranted) {
-      // Request permission if not granted
+
       status = await Permission.camera.request();
 
       if (status.isGranted) {
-        onGranted(); // Start the camera if permission is granted
+        onGranted();
       } else if (status.isDenied) {
         print("Camera permission denied");
       } else if (status.isPermanentlyDenied) {
@@ -16,7 +16,7 @@ class PermissionHandler {
         openAppSettings();
       }
     } else {
-      onGranted(); // Start the camera immediately if permission is already granted
+      onGranted();
     }
   }
 }
