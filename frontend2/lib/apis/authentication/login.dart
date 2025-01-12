6c97c9521703e3c683f65dd61571be2d9b09f382
yamiamit <way2aamit@gmail.com>
2025-01-12 16:20:39 +0530

    added frontend2

---
+++ frontend2/lib/apis/authentication/login.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:http/http.dart' as http;
+import 'package:frontend/screens/profile_screen.dart';
+
+
+
+import 'package:frontend1/constants/endpoint.dart';
+
+import '';
+import 'package:frontend1/apis/users/user.dart';
+import '../../screens/login_screen.dart';
+import 'package:frontend1/apis/protected.dart';
+
+
+Future<void> authenticate() async {
+  try {
+
+    final result = await FlutterWebAuth.authenticate(
+        url: AuthEndpoints.getAccess, callbackUrlScheme: "messchange");
+    print(result);
+
+    final accessToken = Uri.parse(result).queryParameters['token'];
+    print(accessToken);
+
+    final prefs = await SharedPreferences.getInstance();
+
+    if (accessToken == null) {
+      throw ('access token not found');
+    }
+    prefs.setString('access_token', accessToken);
+    await fetchUserDetails();
+
+
+  } on PlatformException catch (_) {
+    rethrow;
+  } catch (e) {
+    print('Error in getting code');
+    rethrow;
+  }
+}
+
+Future<void> logoutHandler(context) async {
+  final prefs = await SharedPreferences.getInstance();
+  final box = await Hive.openBox('coursehub-data');
+
+  prefs.clear();
+  box.clear();
+
+  Navigator.of(context).pushAndRemoveUntil(
+    MaterialPageRoute(
+      builder: (context) => const loginScreen(),
+    ),
+        (route) => false,
+  );
+}
+
+Future<bool> isLoggedIn() async {
+  var access = await getAccessToken();
+
+  if (access != 'error') {
+    return true;
+  } else {
+    return false;
+  }
+}
