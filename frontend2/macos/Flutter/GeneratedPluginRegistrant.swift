d13314e4b1cd14b132179917aaf61ff8d3a2e8d1
Yatika Jena <j.yatika@iitg.ac.in>
2025-01-28 20:05:37 +0530

    added apple sign in

--- frontend2/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend2/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import connectivity_plus
 import flutter_web_auth
 import path_provider_foundation
 import shared_preferences_foundation
+import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
