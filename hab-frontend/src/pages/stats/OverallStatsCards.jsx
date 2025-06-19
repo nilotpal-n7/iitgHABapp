fb9cdfbb6f470df5b586d414c000a31302b7d160
DreamBot706 <187842058+DreamBot706@users.noreply.github.com.>
2025-06-19 17:40:51 +0000

    Complete UI design for overallstatistics pages
    Completed the design and added basic color code logic and statistic display logic.

--- hab-frontend/src/pages/stats/OverallStatsCards.jsx
+++ hab-frontend/src/pages/stats/OverallStatsCards.jsx
@@ -1,41 +1,53 @@
 import { BarChartOutlined, CalendarOutlined, TeamOutlined } from "@ant-design/icons";
 
-function OverallStatsCards() {
-    return (
-        <section className="flex gap-8 mt-8">
+function OverallStatsCards({ stats }) {
 
-          <div className="flex flex-1 gap-4 bg-blue-50 p-6 items-center rounded-2xl">
-            <div className="p-2 bg-blue-100 rounded-lg">
-              <TeamOutlined className="!text-blue-500 text-2xl" />
-            </div>
-            <div>
-              <div className="text-sm text-gray-600">Total Students</div>
-              <div className="text-xl font-bold">77</div>
-            </div>
-          </div>
+  let sum = 0;
+  let first = "breakfast";
+  for(const key in stats){
+    if (key != "total"){
+      sum += stats[key];
+      if(stats[key] > stats[first]) first = key;
+    } 
+  }
 
-          <div className="flex flex-1 gap-4 bg-green-50 p-6 items-center rounded-2xl">
-            <div className="p-2 bg-green-100 rounded-lg">
-              <BarChartOutlined className="!text-green-500 text-2xl" />
-            </div>
-            <div>
-              <div className="text-sm text-gray-600">Total Attendance</div>
-              <div className="text-xl font-bold">{"71.2%"}</div>
-            </div>
-          </div>
+  const attendance = ((sum / stats.total / 3) * 100).toFixed(1).toString() + "%";
 
-          <div className="flex flex-1 gap-4 bg-purple-50 p-6 items-center rounded-2xl">
-            <div className="p-2 bg-purple-100 rounded-lg">
-              <CalendarOutlined className="!text-purple-500 text-2xl" />
-            </div>
-            <div>
-              <div className="text-sm text-gray-600">Most Active Meal</div>
-              <div className="text-xl font-bold">Lunch</div>
-            </div>
-          </div>
+  return (
+    <section className="flex gap-8 mt-8 flex-col md:flex-row">
 
-        </section>
-    )
+      <div className="flex flex-1 gap-4 bg-blue-50 p-4 items-center rounded-2xl">
+        <div className="p-2 bg-blue-100 rounded-lg">
+          <TeamOutlined className="!text-blue-500 text-2xl" />
+        </div>
+        <div>
+          <div className="text-sm text-gray-600">Total Students</div>
+          <div className="text-xl font-bold">{stats.total}</div>
+        </div>
+      </div>
+
+      <div className="flex flex-1 gap-4 bg-green-50 p-4 items-center rounded-2xl">
+        <div className="p-2 bg-green-100 rounded-lg">
+          <BarChartOutlined className="!text-green-500 text-2xl" />
+        </div>
+        <div>
+          <div className="text-sm text-gray-600">Total Attendance</div>
+          <div className="text-xl font-bold">{attendance}</div>
+        </div>
+      </div>
+
+      <div className="flex flex-1 gap-4 bg-purple-50 p-4 items-center rounded-2xl">
+        <div className="p-2 bg-purple-100 rounded-lg">
+          <CalendarOutlined className="!text-purple-500 text-2xl" />
+        </div>
+        <div>
+          <div className="text-sm text-gray-600">Most Active Meal</div>
+          <div className="text-xl font-bold">{first.charAt(0).toUpperCase() + first.slice(1)}</div>
+        </div>
+      </div>
+
+    </section>
+  )
 }
 
 export default OverallStatsCards;
