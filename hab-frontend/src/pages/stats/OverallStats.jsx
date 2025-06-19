bf5257e43554e25b040f8f4b96c6cb0043231462
DreamBot706 <187842058+DreamBot706@users.noreply.github.com.>
2025-06-19 17:40:51 +0000

    Complete UI design for overallstatistics pages
    Completed the design and added basic color code logic and statistic display logic.

--- hab-frontend/src/pages/stats/OverallStats.jsx
+++ hab-frontend/src/pages/stats/OverallStats.jsx
@@ -1,22 +1,21 @@
-import { 
-  BarChartOutlined, 
-  CalendarOutlined,
-  TeamOutlined, 
-} from "@ant-design/icons"
-import { Divider, DatePicker } from "antd";
-
 import OverallStatsHeader from "./OverallStatsHeader";
 import OverallStatsCards from "./OverallStatsCards";
+import MealStatistic from "./MealStatistic";
 
 
 function OverallStats() {
+
   return (
     <div className="p-4 min-h-full m-auto">
       <div className="m-auto p-1 min-h-screen max-w-300 bg-inherit">
 
         <OverallStatsHeader />
 
-        <OverallStatsCards />
+        <OverallStatsCards stats={{breakfast: 22, lunch: 45, dinner: 33, total: 77}}/>
+
+        <MealStatistic meal="Breakfast" date="2025-06-19" stats={{breakfast: 22, lunch: 45, dinner: 33, total: 77}} />
+        <MealStatistic meal="Lunch" date="2025-06-19" stats={{breakfast: 22, lunch: 45, dinner: 33, total: 77}} />
+        <MealStatistic meal="Dinner" date="2025-06-19" stats={{breakfast: 22, lunch: 45, dinner: 33, total: 77}} />
 
       </div>
     </div>
