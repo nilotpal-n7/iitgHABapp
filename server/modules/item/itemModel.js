f479161ab688518376326deb5b8a5f7414a30245
aditya-samal <samaladitya2004@gmail.com>
2025-06-21 18:51:35 +0530

    Final Testing

--- server/modules/item/itemModel.js
+++ server/modules/item/itemModel.js
@@ -1,51 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const itemSchema = new mongoose.Schema(
+const itemSchema = new mongoose.Schema({
+  qrCode: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["unresolved", "resolved", "in_progress"],
+  },
+  complainUpdate: {
+    type: String,
+  },
+  complaints: [
     {
-        qrCode: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        name: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String
-        },
-        location: {
-            type: String
-        },
-        status: {
-            type: String,
-            enum: ['unresolved', 'resolved', 'in_progress']  
-        },
-        complainUpdate: {
-            type: String
-        },
-        complaints: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Complaint' 
-        }],
-        currentAuthority: {
-            // type: mongoose.Schema.Types.ObjectId,
-            // ref: 'User'
-            type: String,
-            enum: ['hab', 'secy'],
-            default: 'secy'
-        },
-        hostel: {
-            type: String
-        }
-        ,
-        comment:{
-            type :String
-        }
-    }
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Complaint",
+    },
+  ],
+  currentAuthority: {
+    // type: mongoose.Schema.Types.ObjectId,
+    // ref: 'User'
+    type: String,
+    enum: ["hab", "secy"],
+    default: "secy",
+  },
+  hostel: {
+    type: String,
+  },
+  comment: {
+    type: String,
+  },
+});
 
-);
-
-const Item = mongoose.model('Item', itemSchema);
+const Item = mongoose.model("Item", itemSchema);
 
 module.exports = Item;
