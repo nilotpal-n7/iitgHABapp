1dc11621fee438dc4bb49cd24b9026da597d5c0e
Jayansh <jdgupta1980@gmail.com>
2025-08-05 02:07:51 +0530

    Mess Change feature added in hmc frontend

--- server/modules/mess_change/messchangeModel.js
+++ server/modules/mess_change/messchangeModel.js
@@ -3,42 +3,47 @@ const mongoose = require("mongoose");
 const messchangeSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    
+
   },
   name: {
     type: String,
-    
+
   },
   rollNumber: {
     type: String,
-    unique : true,
-    required : true
+    unique: true,
+    required: true
   },
   degree: {
     type: String,
-    
+
   },
   hostelId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Hostel",
-      
-    },
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hostel",
+
+  },
   sec_pref_mess: {
     type: String,
-   
+
   },
-    next_mess: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Hostel",
-      default: function () {
-        return this.hostel;
-      },
+  next_mess: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hostel",
+    default: function () {
+      return this.hostel;
     },
-    got_mess_changed: {
+  },
+  got_mess_changed: {
     type: Boolean,
     default: false,
   },
-  
+   status: {
+    type: String,
+    enum: ['pending', 'approved', 'rejected'],
+    default: 'pending',
+  },
+
 });
 
 const MessChange = mongoose.model("MessChange", messchangeSchema);
