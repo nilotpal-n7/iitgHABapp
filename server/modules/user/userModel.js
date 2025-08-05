9b2901e103782db3c449d82eb5fd49147e5117c6
TheInfamousTheta <g.kaustubh@iitg.ac.in>
2025-08-05 21:38:50 +0530

    test push

--- server/modules/user/userModel.js
+++ server/modules/user/userModel.js
@@ -143,6 +143,11 @@ const userSchema = new mongoose.Schema({
     default: false,
   },
 
+  applied_hostel_timestamp: {
+    type: Date,
+    default: new Date('2024-8-5'),
+  }
+
   applied_hostel_string: {
     type: String,
     default: "",
