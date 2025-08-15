01fbd9e76ecc317e1d6f70fd6a54681ae3033b36
piyush-kumar-kedia <piyushkumarkedia02221@gmail.com>
2025-08-15 14:45:48 +0530

    added hostel wise stats

--- hab-frontend/src/pages/HostelPage.jsx
+++ hab-frontend/src/pages/HostelPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import HostelStats from './stats/HostelStats';
 
 export default function HostelPage() {
   const { hostelId } = useParams();
@@ -232,6 +233,7 @@ export default function HostelPage() {
         </div>
 
         {error && <div className="text-red-500">{error}</div>}
+        {messDetails && (<HostelStats />)}
         </div>
       </div>
       );
