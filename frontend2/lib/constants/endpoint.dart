69d49d84179408dd6833c86516e1aa6be2d98ec1
aditya-samal <samaladitya2004@gmail.com>
2025-10-21 01:49:43 +0530

    Mess Change Done

--- frontend2/lib/constants/endpoint.dart
+++ frontend2/lib/constants/endpoint.dart
@@ -1,4 +1,4 @@
-const String baseUrl = "https://hab.codingclub.in/api";
+const String baseUrl = "http://localhost:3000/api";
 
 class NotificationEndpoints {
   static const String registerToken = '$baseUrl/notification/register-token';
@@ -41,27 +41,22 @@ class Userendpoints {
   static const getdetails = 'https://graph.microsoft.com/v1.0/me';
 }
 
-
 class tokenlink {
   static const Tokenlink =
       'https://login.microsoftonline.com/850aa78d-94e1-4bc6-9cf3-8c11b530701c/oauth2/v2.0/token';
 }
 
 class redirecturi {
-  static const Redirecturi =
-      '${baseUrl}/auth/login/redirect/mobile';
+  static const Redirecturi = '${baseUrl}/auth/login/redirect/mobile';
 }
 
-
-
 class messFeedback {
-  static const feedbackSubmit =
-      "$baseUrl/feedback/submit";
+  static const feedbackSubmit = "$baseUrl/feedback/submit";
 }
 
 class messInfo {
   static const getMessInfo = "$baseUrl/mess/all";
-  static const getUserMessInfo ="$baseUrl/mess/get";
+  static const getUserMessInfo = "$baseUrl/mess/get";
 }
 
 class ProfilePicture {
