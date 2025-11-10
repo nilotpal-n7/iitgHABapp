77ddc9aa8e4e83e10938eaede13b5be5d92efb22
aditya-samal <samaladitya2004@gmail.com>
2025-11-10 13:44:45 +0530

    Redirection fix Attempt 2

--- hab-frontend/vite.config.js
+++ hab-frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/hab/", // 👈 CRITICAL FIX — ensures asset URLs are /hab/assets/*
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
