48dee42731d2bd143332b021bb3f869076f4200f
aditya-samal <samaladitya2004@gmail.com>
2025-11-10 13:41:07 +0530

    Revert

--- hab-frontend/src/context/AuthProvider.jsx
+++ hab-frontend/src/context/AuthProvider.jsx
@@ -7,7 +7,7 @@ const AuthContext = createContext();
 
 const BACKEND_URL =
   import.meta.env.VITE_SERVER_URL || "http://localhost:3000/api";
-const APP_URL = "https://youtube.com";
+const APP_URL = import.meta.env.VITE_APP_URL || "http://localhost:5172";
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token") || null);
